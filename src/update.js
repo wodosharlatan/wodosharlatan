1ac5f79db045a651d2efaea4ca33ca34a6311657
Tomáš Bosák <85733005+wodosharlatan@users.noreply.github.com>
2023-03-27 11:34:48 +0200

    Update update.js

--- src/update.js
+++ src/update.js
@@ -84,7 +84,7 @@ const Tom = {
   <img src="https://github.com/devicons/devicon/blob/master/icons/php/php-original.svg" title="PHP" alt="PHP" width="40" height="40"/>&nbsp;
   <img src="https://github.com/devicons/devicon/blob/master/icons/mysql/mysql-original.svg" title="MySql" alt="MySql" width="40" height="40"/>&nbsp;
   <img src="https://github.com/devicons/devicon/blob/master/icons/linux/linux-original.svg" title="Linux" alt="Linux" width="40" height="40"/>&nbsp;
-  <img src="https://github.com/devicons/devicon/blob/master/icons/node/nodejs-original-wordmark.svg" title="Node" alt="NodeJS" width="40" height="40"/>&nbsp;
+ 
 </div>
 
 
